d7b3db57ed7509290281d98606eeb92b7db77bb9
noload <vaibhavkamble023@gmail.com>
2024-02-08 19:50:18 +0530

    Added category filter function

--- product-ecom/src/App.jsx
+++ product-ecom/src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./component/Home";
 import Details from "./component/Details";
 function App() {
+  const { pathname, search } = useLocation();
+  console.log(pathname, search);
   return (
     <>
-      <div className="w-screen h-screen flex">
+      <div className="w-screen  h-screen flex">
+        {(pathname != "/" || search.length > 0) && (
+          <Link
+            to="/"
+            className="absolute left-56 top-5 px-3 py-1 font-semibold text-white bg-blue-400 rounded-md hover:bg-blue-500"
+          >
+            Home
+          </Link>
+        )}
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/details/:id" element={<Details />} />
--- product-ecom/src/component/Home.jsx
+++ product-ecom/src/component/Home.jsx
@@ -1,33 +1,55 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Nav from "./Nav";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ProductContext } from "../utils/Context";
 import Loading from "./Loading";
-
+import axios from "../utils/axios";
 const Home = () => {
+  //to get query params
+  const { search } = useLocation();
+  const category = decodeURIComponent(search.split("=")[1]);
   const [product, setProduct] = useContext(ProductContext);
+  const [filterProduct, setFilterProduct] = useState(null);
+  const getProductsByCategory = async () => {
+    try {
+      const { data } = await axios.get(`/products/category/${category}`);
+      setFilterProduct(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!filterProduct || category == "undefined") {
+      setFilterProduct(product);
+    }
+    if (category != "undefined") {
+      getProductsByCategory();
+    }
+  }, [category, product]);
   return (
     <>
       <Nav />
       {product ? (
         <div className="h-full w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
-          {product.map((p, i) => {
-            return (
-              <Link
-                key={i}
-                to={`/details/${p.id}`}
-                className="card p-3 border shadow rounded w-[18%] h-[40vh] flex flex-col justify-center items-center mr-3 mb-3"
-              >
-                <div
-                  className="w-full h-[80%] bg-contain bg-no-repeat bg-center hover:scale-110 transition-all ease-in-out duration-300"
-                  style={{
-                    backgroundImage: `url(${p.image})`,
-                  }}
-                ></div>
-                <h1 className="hover:text-blue-300">{p.title}</h1>
-              </Link>
-            );
-          })}
+          {filterProduct &&
+            filterProduct.map((p, i) => {
+              return (
+                <Link
+                  key={i}
+                  to={`/details/${p.id}`}
+                  className="card p-3 border shadow rounded w-[18%] h-[40vh] flex flex-col justify-center items-center mr-3 mb-3"
+                >
+                  <div
+                    className="w-full h-[80%] bg-contain bg-no-repeat bg-center hover:scale-110 transition-all ease-in-out duration-300"
+                    style={{
+                      backgroundImage: `url(${p.image})`,
+                    }}
+                  ></div>
+                  <h1 className="hover:text-blue-300">{p.title}</h1>
+                </Link>
+              );
+            })}
         </div>
       ) : (
         <Loading />
